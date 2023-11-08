8c55b9f15b970a75dd32ed2dedbae18d631ea94d
Tsaplin Sergey <74894518+talatari@users.noreply.github.com>
2023-11-08 13:31:49 +0300

    Configure parameters fish

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
-    [SerializeField, Range(1, 100)] private int _timeLife = 10;
+    [SerializeField, Range(1, 50)] private int _timeLife = 7;
     
     private void Start() => Destroy(gameObject, _timeLife);
 }
